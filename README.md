0a574dc4a763ba8d5ee1ebbe34655aaa1c39790c
wangfuling <wang.fuling@fatalent.com>
2023-09-08 10:02:08 +0800

    添加文本高亮显示

--- README.md
+++ README.md
@@ -20,11 +20,30 @@ npm i --save @kne/mini-resume
 
 - 这里填写示例标题
 - 这里填写示例说明
-- 
+- miniResume(@kne/mini-resume),miniCore(@kne/mini-core),lodash(lodash)
 
 ```jsx
-const BaseExample = ()=>{
-    return '我是一个示例组件';
+const { ResumeCard } = miniResume;
+const {HighLightProvider} = miniCore;
+
+const BaseExample = () => {
+  return <HighLightProvider keyword="年">
+    <ResumeCard resumeData={{
+      "age": 32,
+      "birth": "1991-07-18T14:28:01.230+08:00",
+      "birthFromAge": true,
+      "candidateUid": null,
+      "card": "898765367444776589",
+      "cardType": 1,
+      "name": "张三",
+      "phone": null,
+      "degree": 60,
+      "gender": "F",
+      "currentLocation": "上海",
+      "workExperience": 10,
+      "photo": null
+    }} />
+  </HighLightProvider>;
 };
 
 render(<BaseExample />);
--- src/ResumeCard.js
+++ src/ResumeCard.js
@@ -1,54 +1,77 @@
-import React from 'react';
-import style from './card.module.scss';
-import {View, Image, Text} from '@tarojs/components';
-import classnames from 'classnames';
-import {Enum, CommonCityEnum, AvatarPreview} from '@kne/mini-core';
-import {Space} from '@kne/antd-taro';
-import dayjs from 'dayjs';
-import beiwosuoding from './assets/beiwosuoding.svg';
-import jinzhisuoding from './assets/jinzhisuoding.svg';
-import yisuoding from './assets/yisuoding.svg';
-import femaleSrc from './assets/female.svg';
-import maleSrc from './assets/male.svg';
-import {isNil} from 'lodash';
+import React from "react";
+import style from "./card.module.scss";
+import { Image, View } from "@tarojs/components";
+import classnames from "classnames";
+import { AvatarPreview, CommonCityEnum, Enum, HighLight } from "@kne/mini-core";
+import { Space } from "@kne/antd-taro";
+import dayjs from "dayjs";
+import beiwosuoding from "./assets/beiwosuoding.svg";
+import jinzhisuoding from "./assets/jinzhisuoding.svg";
+import yisuoding from "./assets/yisuoding.svg";
+import femaleSrc from "./assets/female.svg";
+import maleSrc from "./assets/male.svg";
+import { get, isNil } from "lodash";
 
-const ResumeCard = ({resumeData, className, lockRender, userInfo, nameExtra}) => {
-  const {id, name, phone, degree, gender, age, currentLocation = "上海", workExperience, photo, resumeLock} = resumeData
-  const {uid, locked, expireAt} = resumeLock || {};
+const ResumeCard = ({ resumeData, className, lockRender, userInfo, nameExtra }) => {
+  const {
+    id,
+    name,
+    phone,
+    degree,
+    gender,
+    age,
+    currentLocation = "上海",
+    workExperience,
+    photo,
+    resumeLock
+  } = resumeData;
+  const { uid, locked, expireAt } = resumeLock || {};
   const isByMeLock = userInfo?.appUserInfo?.uid === uid && locked;
-  const isBanLock = !locked && !!expireAt && new Date(dayjs(expireAt).format('YYYY-MM-DD HH:mm:ss')).getTime() > new Date().getTime();
+  const isBanLock = !locked && !!expireAt && new Date(dayjs(expireAt).format("YYYY-MM-DD HH:mm:ss")).getTime() > new Date().getTime();
   const lockIconSrc = locked ? (isByMeLock ? beiwosuoding : yisuoding) : isBanLock ? jinzhisuoding : null;
   const lockIconText = locked ? (isByMeLock ? "被我锁定" : "已锁定") : isBanLock ? "禁止锁定" : null;
 
-  return <View className={classnames(style['basic'], className)}>
-    <View className={classnames(style['flex'], style['basic-top'])}>
-      <View className={classnames(style['avatar'],"avatar")}>
-        {photo ? <AvatarPreview value={photo}/> : <Image className={style['avatar-icon']} src={gender === 'F' ? femaleSrc : maleSrc}/>}
+  return <View className={classnames(style["basic"], className)}>
+    <View className={classnames(style["flex"], style["basic-top"])}>
+      <View className={classnames(style["avatar"], "avatar")}>
+        {photo ? <AvatarPreview value={photo} /> :
+          <Image className={style["avatar-icon"]} src={gender === "F" ? femaleSrc : maleSrc} />}
       </View>
-      <View className={style['info']}>
-         <View className={classnames(style['name-box'],{
-           [style['padding-right']]:!!nameExtra
-         },"name-box")}>
-          <Text className={classnames(style['name'], "name")}>{name||"缺失姓名"}</Text>
-          {lockRender && lockIconSrc && lockRender(lockIconSrc,lockIconText)}
-           {nameExtra}
+      <View className={style["info"]}>
+        <View className={classnames(style["name-box"], {
+          [style["padding-right"]]: !!nameExtra
+        }, "name-box")}>
+          <HighLight text={name || "缺失姓名"} className={classnames(style["name"], "name")} />
+          {lockRender && lockIconSrc && lockRender(lockIconSrc, lockIconText)}
+          {nameExtra}
         </View>
-        <Space wrap={false} style={{'--gap': '0'}} className={style['basic-info']}>
-          {gender && <Enum moduleName={"gender"} name={gender}/>}
-          {age > 0 && <View>{age}岁</View>}
-          {currentLocation && <View className={style['city']}><CommonCityEnum name={currentLocation}/></View>}
-          {!isNil(degree) && degree > 0 ? <Enum moduleName={"degreeEnum"} name={degree}/> : null}
-          {!isNil(workExperience) && workExperience > 0 ? <View>{workExperience}年</View> : null}
+        <Space wrap={false} style={{ "--gap": "0" }} className={style["basic-info"]}>
+          {gender && <Enum moduleName={"gender"} name={gender}>
+            {(item) => <HighLight text={get(item, "description", "")} />}
+          </Enum>}
+          {age > 0 && <HighLight tagName={View} text={`${age}岁`} />}
+          {currentLocation && <View className={style["city"]}>
+            <CommonCityEnum name={currentLocation}>
+              {(item) => {
+                return <HighLight text={get(item, "city.name", "")} />;
+              }}
+            </CommonCityEnum>
+          </View>}
+          {!isNil(degree) && degree > 0 ? <Enum moduleName={"degreeEnum"} name={degree}>
+            {(item) => <HighLight text={get(item, "description", "")} />}
+          </Enum> : null}
+          {!isNil(workExperience) && workExperience > 0 ?
+            <HighLight tagName={View} text={`${workExperience}年`} /> : null}
         </Space>
       </View>
     </View>
-  </View>
-}
+  </View>;
+};
 
-ResumeCard.defaultProps={
-  lockRender:(src)=>{
-    return <Image className={style['lock-icon']} src={src}/>
+ResumeCard.defaultProps = {
+  lockRender: (src) => {
+    return <Image className={style["lock-icon"]} src={src} />;
   }
-}
+};
 
-export default ResumeCard
+export default ResumeCard;
