62b34ebf087dbc3f96e36fbcd4c87c706ea228d5
jinfanghuang <2283785225@qq.com>
2023-12-06 13:27:41 +0800

    feat: 修改使用的 Mini-core 组件

--- src/ResumeForm/ListOptions.js
+++ src/ResumeForm/ListOptions.js
@@ -5,14 +5,14 @@ import Taro from "@tarojs/taro";
 import {View} from "@tarojs/components";
 import {deleteResumeList} from "./resumeApi";
 import style from './style.module.scss';
-import {Button, FixView, usePreset} from '@kne/mini-core'
+import {FixedButton, FixedView, usePreset} from '@kne/mini-core'
 
 const ListOptions = ({index, name, data, label, onSuccess }) => {
 const {apis,ajax}=usePreset();
-  return  <FixView className={style['btn-list']}>
+  return  <FixedView>
     {
    (Number.isInteger(+index) && get(data,`${name}.length`,0) > 1) ? <>
-          <Button className={style['flex-1']} type="default" onClick={() => {
+          <FixedButton className={style['flex-1']} type="default" onClick={() => {
             Taro.showModal({
               title: `是否删除该${label}`,
               cancelText:"取消",
@@ -26,18 +26,18 @@ const {apis,ajax}=usePreset();
             })
           }}>
             删除
-          </Button>
-          <Button className={style['flex-2']} type="primary">
+          </FixedButton>
+          <FixedButton className={style['flex-2']} type="primary">
             <SubmitButton block type="primary">
               <View id={`save_${name}`}>保存</View>
             </SubmitButton>
-        </Button>
+        </FixedButton>
         </>:
     <SubmitButton block type="primary" className="submit-button">
       <View id={`save_${name}`}>保存</View>
     </SubmitButton>
 }
-  </FixView>
+  </FixedView>
 };
 
 export default ListOptions;
